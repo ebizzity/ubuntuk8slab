5460588b00e3991207c9fb2f9b7d722490a138a0
Erik Bailey <erbail@microsoft.com>
2020-04-10 10:47:57 -0400

    updated setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
+
+#INSTALL Packages
 apt-get update
 echo virtualbox-ext-pack virtualbox-ext-pack/license select true | sudo debconf-set-selections
-apt-get install -y apt-transport-https virtualbox virtualbox-ext-pack xfce4 xrdp 
-echo xfce4-session >/home/student/.xsession
-echo xfce4-session >~/.xsession
-systemctl enable xrdp
+apt-get install -y \
+apt-transport-https \
+virtualbox \
+virtualbox-ext-pack \
+mate-core \
+mate-desktop-environment \
+mate-notification-daemon
+indicator-sound-gtk2 \
+indicator-application-gtk2 \
+xrdp 
 
 # allow just RDP through the local firewall
 ufw allow 3389/tcp
@@ -15,6 +23,8 @@ ufw allow 3389/tcp
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 touch /etc/apt/sources.list.d/kubernetes.list 
 echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | tee -a /etc/apt/sources.list.d/kubernetes.list
+
+#INSTALL kubectl
 apt-get update
 apt-get install -y kubectl
 
